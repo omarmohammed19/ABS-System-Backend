9062f0de645045c00094dd5b5ac6c3a1f6155aff
Hamdi-Hossam <hamdihossam461@gmail.com>
2022-08-01 16:47:08 +0200

    telephoneTypes added

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import test_Routes from './Controllers/test';
 import servicesTypes_endpoints from './Routes/servicesTypesRoute';
 import subAccount_endpoints from './Routes/subAccountRoute';
 import telephoneNumber_endpoints from './Routes/telephoneNumberRoute';
+import telephoneTypes_endpoints from './Routes/telephoneTypesRoute';
 
 const app: express.Application = express();
 const address: string = 'localhost';
@@ -16,6 +17,7 @@ test_Routes(app);
 servicesTypes_endpoints(app);
 subAccount_endpoints(app);
 telephoneNumber_endpoints(app);
+telephoneTypes_endpoints(app);
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port http://${address}:${process.env.PORT}`);
 });
