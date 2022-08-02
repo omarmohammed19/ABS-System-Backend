f4370d4b5e8ea0663f2bc5b198fdb6569f159d83
adhamahmed4 <adhamahmeds2312@gmail.com>
2022-08-02 17:59:29 +0200

    Remove test routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express from 'express';
-import test_Routes from './Controllers/test';
 import servicesTypes_endpoints from './Routes/servicesTypesRoute';
 import subAccount_endpoints from './Routes/subAccountRoute';
 import telephoneNumber_endpoints from './Routes/telephoneNumberRoute';
@@ -36,7 +35,6 @@ const address: string = 'localhost';
 dotenv.config();
 
 app.use(bodyParser.json());
-test_Routes(app);
 servicesTypes_endpoints(app);
 subAccount_endpoints(app);
 telephoneNumber_endpoints(app);
