474a9092bdcd2ea439231a98019b5d636ef8b684
Hamdi-Hossam <hamdihossam461@gmail.com>
2022-08-29 17:21:25 +0200

    crash handeled

--- src/Routes/uploadFilesRoute.ts
+++ src/Routes/uploadFilesRoute.ts
@@ -4,21 +4,25 @@ import upload from '../Controllers/uploadFilesController';
 const imageRouter = Router();
 import fs from 'fs';
 
-imageRouter.post('/single',upload.single('file'), (req, res) => {
-    //@ts-ignore
-    res.json({success: true});
-})
+imageRouter.post('/single', upload.single('file'), (req, res) => {
+  //@ts-ignore
+  res.json({ success: true });
+});
 
-imageRouter.post('/multiple',upload.array('files', 12), (req, res) => {
-    res.send('Multiple files uploaded');
-})
+imageRouter.post('/multiple', upload.array('files', 12), (req, res) => {
+  res.send('Multiple files uploaded');
+});
 
 imageRouter.get('/getImage', (req, res) => {
+  try {
     fs.readFile('uploads/' + req.query.name, (err, data) => {
-        if (err) throw err;
-        res.writeHead(200);
-        res.end(data);
-    })
-})
+      // if (err) throw err;
+      res.writeHead(200);
+      res.end(data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export default imageRouter;
