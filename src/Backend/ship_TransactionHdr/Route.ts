103ab64bc0842a5421bad1f0d27241016876169a
omarmohammed19 <75382998+omarmohammed19@users.noreply.github.com>
2023-04-10 21:50:29 +0200

    ship_Transactions ship_TransactionHdr additional APIs

--- src/Backend/ship_TransactionHdr/Route.ts
+++ src/Backend/ship_TransactionHdr/Route.ts
@@ -23,7 +23,7 @@ const getById = async (req: Request, res: Response) => {
     const result = await transactionHdrController.getTransactionHdrByID(Number(req.params.ID), language);
     res.json(result);
   } catch (error) {
-    console.log("ID:" + error);
+    console.log('ID:' + error);
     res.status(400);
     res.json(error);
   }
@@ -35,7 +35,7 @@ const getBySubAccountId = async (req: Request, res: Response) => {
     const result = await transactionHdrController.getTransactionHdrBySubAccountID(Number(req.params.subAccountID), language);
     res.json(result);
   } catch (error) {
-    console.log("subID:" + error);
+    console.log('subID:' + error);
     res.status(400);
     res.json(error);
   }
@@ -47,7 +47,7 @@ const getByMainAccountId = async (req: Request, res: Response) => {
     const result = await transactionHdrController.getTransactionHdrByMainAccountID(Number(req.params.mainAccountID), language);
     res.json(result);
   } catch (error) {
-    console.log("mainID:" + error);
+    console.log('mainID:' + error);
     res.status(400);
     res.json(error);
   }
@@ -92,9 +92,9 @@ const update = async (req: Request, res: Response) => {
   }
 };
 
-const deactivate = async (req: Request, res: Response) => {
+const deactivateByID = async (req: Request, res: Response) => {
   try {
-    const result = await transactionHdrController.deactivate(Number(req.params.transactionHdrID));
+    const result = await transactionHdrController.deactivateByID(Number(req.params.ID));
     res.json(result);
   } catch (error) {
     console.log(error);
@@ -103,9 +103,51 @@ const deactivate = async (req: Request, res: Response) => {
   }
 };
 
-const activate = async (req: Request, res: Response) => {
+const activateByID = async (req: Request, res: Response) => {
   try {
-    const result = await transactionHdrController.activate(Number(req.params.transactionHdrID));
+    const result = await transactionHdrController.activateByID(Number(req.params.ID));
+    res.json(result);
+  } catch (error) {
+    res.status(400);
+    res.json(error);
+  }
+};
+
+const deactivateBySubAccountID = async (req: Request, res: Response) => {
+  try {
+    const result = await transactionHdrController.deactivateBySubAccountID(Number(req.params.SubAccountID));
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+    res.json(error);
+  }
+};
+
+const activateBySubAccountID = async (req: Request, res: Response) => {
+  try {
+    const result = await transactionHdrController.activateBySubAccountID(Number(req.params.SubAccountID));
+    res.json(result);
+  } catch (error) {
+    res.status(400);
+    res.json(error);
+  }
+};
+
+const deactivateByMainAccountID = async (req: Request, res: Response) => {
+  try {
+    const result = await transactionHdrController.deactivateByMainAccountID(Number(req.params.MainAccountID));
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+    res.json(error);
+  }
+};
+
+const activateByMainAccountID = async (req: Request, res: Response) => {
+  try {
+    const result = await transactionHdrController.activateByMainAccountID(Number(req.params.MainAccountID));
     res.json(result);
   } catch (error) {
     res.status(400);
@@ -120,8 +162,12 @@ const transactionHdrRouter = (app: express.Application) => {
   app.get('/transactionHdr-by-mainAccountID/:mainAccountID', getByMainAccountId);
   app.post('/transactionHdr', create);
   app.put('/transactionHdr/:ID', update);
-  app.put('/transactionHdr/deactivate/:transactionHdrID', deactivate);
-  app.put('/transactionHdr/activate/:transactionHdrID', activate);
+  app.put('/transactionHdr/de-activate-by-ID/:ID', deactivateByID);
+  app.put('/transactionHdr/activate-by-ID/:ID', activateByID);
+  app.put('/transactionHdr/de-activate-by-subAccountID/:SubAccountID', deactivateBySubAccountID);
+  app.put('/transactionHdr/activate-by-subAccountID/:SubAccountID', activateBySubAccountID);
+  app.put('/transactionHdr/de-activate-by-mainAccountID/:MainAccountID', deactivateByMainAccountID);
+  app.put('/transactionHdr/activate-by-mainAccountID/:MainAccountID', activateByMainAccountID);
 };
 
 export default transactionHdrRouter;
