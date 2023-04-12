533c88c02d98fd2f72c166588acfdd5b4d538f7b
omarmohammed19 <75382998+omarmohammed19@users.noreply.github.com>
2023-04-12 15:58:18 +0200

    Create Shipments Create Pickups APIs

---
+++ src/ClientServices/Create Shipments/Route.ts
@@ -0,0 +1,118 @@
+import express, { Request, Response } from 'express';
+import { CreateSingleShipmentController } from './Controller';
+import { Sequelize } from 'sequelize';
+import { TransactionHdrModel } from '../../Backend/ship_TransactionHdr/Model';
+import { PickupsModel } from '../../Backend/ship_Pickups/Model';
+import { PickupHistoryModel } from '../../Backend/ship_PickupHistory/Model';
+import { TransactionsModel } from '../../Backend/ship_Transactions/Model';
+import { TransactionHistoryModel } from '../../Backend/ship_TransactionHistory/Model';
+import { ContactPersonsModel } from '../../Backend/cnee_ContactPersons/Model';
+import { ContactNumbersModel } from '../../Backend/cnee_ContactNumbers/Model';
+import { AddressesModel } from '../../Backend/cnee_Addresses/Model';
+
+const createSingleShipmentController = new CreateSingleShipmentController();
+
+const createSingleShipment = async (req: Request, res: Response) => {
+  try {
+    const currentDate = Sequelize.literal('GETDATE()');
+    const expectedDeliveryDate = Sequelize.literal('GETDATE()+1');
+    const expiryDate = Sequelize.literal('GETDATE()+3');
+    const transactionHdr = <TransactionHdrModel>(<unknown>{
+      mainAccountID: req.body.mainAccountID,
+      subAccountID: req.body.subAccountID,
+      userID: req.body.userID,
+      creationDate: currentDate,
+      noOfAWBs: req.body.noOfAWBs,
+    });
+
+    const pickup = <PickupsModel>(<unknown>{
+      mainAccountID: req.body.mainAccountID,
+      subAccountID: req.body.subAccountID,
+      pickupLocationID: req.body.pickupLocationID,
+      pickupTypeID: req.body.pickupTypeID,
+      vehicleTypeID: req.body.vehicleTypeID,
+      noOfAWBs: req.body.noOfAWBs,
+      actualAWBs: req.body.actualAWBs,
+      timeFrom: req.body.timeFrom,
+      toTime: req.body.toTime,
+      userID: req.body.userID,
+      creationDate: currentDate,
+      Notes: req.body.pickupNotes,
+    });
+
+    const pickupHistory = <PickupHistoryModel>(<unknown>{
+      actionTime: currentDate,
+      userID: req.body.userID,
+    });
+
+    const transaction = <TransactionsModel>(<unknown>{
+      Ref: req.body.Ref,
+      mainAccountID: req.body.mainAccountID,
+      subAccountID: req.body.subAccountID,
+      shipmentTypeID: req.body.shipmentTypeID,
+      expectedDeliveryDate: expectedDeliveryDate,
+      productID: req.body.productID,
+      creationDate: currentDate,
+      lastChangeDate: currentDate,
+      userID: req.body.userID,
+      expiryDate: expiryDate,
+      deliveryBranchID: req.body.deliveryBranchID,
+      toBranchID: req.body.toBranchID,
+      specialInstructions: req.body.specialInstructions,
+      packageTypeID: req.body.packageTypeID,
+      noOfPcs: req.body.noOfPcs,
+      contents: req.body.contents,
+      weight: req.body.weight,
+      Cash: req.body.Cash,
+    });
+
+    const transactionHistory = <TransactionHistoryModel>(<unknown>{
+      shipmentTypeID: req.body.shipmentTypeID,
+      auditDate: currentDate,
+      userID: req.body.userID,
+      toBranchID: req.body.toBranchID,
+    });
+
+    const contactPerson = <ContactPersonsModel>(<unknown>{
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+    });
+
+    const contactNumber = <ContactNumbersModel>(<unknown>{
+      contactNumber: req.body.contactNumber,
+      numberTypeID: req.body.numberTypeID,
+    });
+
+    const address = <AddressesModel>(<unknown>{
+      streetName: req.body.streetName,
+      apartmentNumber: req.body.apartmentNumber,
+      floorNumber: req.body.floorNumber,
+      buildingNumber: req.body.buildingNumber,
+      cityID: req.body.cityID,
+      postalCode: req.body.postalCode,
+      longitude: req.body.longitude,
+      latitude: req.body.latitude,
+    });
+
+    const result = await createSingleShipmentController.createShipment(
+      transactionHdr,
+      pickup,
+      pickupHistory,
+      transaction,
+      transactionHistory,
+      contactPerson,
+      contactNumber,
+      address
+    );
+    res.json(result);
+  } catch (error) {
+    console.log('🚀 ~ file: Route.ts:109 ~ createSingleShipment ~ error:', error);
+    res.status(400);
+    res.json(error);
+  }
+};
+
+const CreateShipmentServicesRouter = (app: express.Application) => {
+  app.post('/create-single-shipment', createSingleShipment);
+};
+export default CreateShipmentServicesRouter;
--- src2/Config/database.ts
+++ src2/Config/database.ts
@@ -2,22 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {
-    SQL_HOST,
-    SQL_DB,
-    SQL_USER,
-    SQL_PASSWORD
-} = process.env;
+const { SQL_HOST, SQL_DB2, SQL_USER, SQL_PASSWORD } = process.env;
 
 export const sqlConfig = {
-    user: SQL_USER,
-    password: SQL_PASSWORD,
-    database: SQL_DB,
-    server: SQL_HOST,
-    driver: 'msnodesqlv8',
-}
-
+  user: SQL_USER,
+  password: SQL_PASSWORD,
+  database: SQL_DB2,
+  server: SQL_HOST,
+  driver: 'msnodesqlv8',
+};
 
 export default sqlConfig;
-
-
