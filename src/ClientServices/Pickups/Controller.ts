fbe50fab13b19c6d563a950d9ba8ceca26aad5ac
omarmohammed19 <75382998+omarmohammed19@users.noreply.github.com>
2023-06-14 16:23:07 +0300

    Pickups

--- src/ClientServices/Pickups/Controller.ts
+++ src/ClientServices/Pickups/Controller.ts
@@ -2,35 +2,42 @@ import { PickupsModel } from './Model';
 import { sequelize } from '../../Config/database';
 import Sequelize from 'sequelize';
 
-
 export class PickupsController {
-
-
-    async getHistoryPickups(language: string, subAccountID: number, limits?: number): Promise<PickupsModel[]> {
-        const limit = limits || 10;
-        try {
-            const query = 'EXEC [dbo].[p_Pickups] @language = :language, @Method = :Method, @subAccountID= :subAccountID, @limit = :limit';
-            const replacements = { language: language, Method: 'GET_HistoryPickups', subAccountID: subAccountID, limit: limit };
-            const options = { replacements: replacements, type: Sequelize.QueryTypes.SELECT };
-            const result = await sequelize.query(query, options);
-            return result as unknown as PickupsModel[];
-        }
-        catch (err) {
-            throw new Error(`Could not get History Pickups. Error: ${err}`);
-        }
+  async getHistoryPickups(language: string, subAccountID: number, limits?: number): Promise<PickupsModel[]> {
+    const limit = limits || 10;
+    try {
+      const query = 'EXEC [dbo].[p_Pickups] @language = :language, @Method = :Method, @subAccountID= :subAccountID, @limit = :limit';
+      const replacements = { language: language, Method: 'GET_HistoryPickups', subAccountID: subAccountID, limit: limit };
+      const options = { replacements: replacements, type: Sequelize.QueryTypes.SELECT };
+      const result = await sequelize.query(query, options);
+      return result as unknown as PickupsModel[];
+    } catch (err) {
+      throw new Error(`Could not get History Pickups. Error: ${err}`);
     }
+  }
 
-    async getUpcomingPickups(language: string, subAccountID: number, limits?: number): Promise<PickupsModel[]> {
-        const limit = limits || 10;
-        try {
-            const query = 'EXEC [dbo].[p_Pickups] @language = :language, @Method = :Method, @subAccountID= :subAccountID, @limit = :limit';
-            const replacements = { language: language, Method: 'GET_UpcomingPickups', subAccountID: subAccountID, limit: limit };
-            const options = { replacements: replacements, type: Sequelize.QueryTypes.SELECT };
-            const result = await sequelize.query(query, options);
-            return result as unknown as PickupsModel[];
-        }
-        catch (err) {
-            throw new Error(`Could not get Upcoming Pickups. Error: ${err}`);
-        }
+  async getUpcomingPickups(language: string, subAccountID: number, limits?: number): Promise<PickupsModel[]> {
+    const limit = limits || 10;
+    try {
+      const query = 'EXEC [dbo].[p_Pickups] @language = :language, @Method = :Method, @subAccountID= :subAccountID, @limit = :limit';
+      const replacements = { language: language, Method: 'GET_UpcomingPickups', subAccountID: subAccountID, limit: limit };
+      const options = { replacements: replacements, type: Sequelize.QueryTypes.SELECT };
+      const result = await sequelize.query(query, options);
+      return result as unknown as PickupsModel[];
+    } catch (err) {
+      throw new Error(`Could not get Upcoming Pickups. Error: ${err}`);
     }
+  }
+
+  async getAWBsByPickupID(language: string, subAccountID: number, PickupID: number): Promise<PickupsModel[]> {
+    try {
+      const query = 'EXEC [dbo].[p_GET_ship_Pickups] @language = :language, @Method = :Method, @subAccountID= :subAccountID, @PickupID = :PickupID';
+      const replacements = { language: language, Method: 'GET_AWBsByPickupID', subAccountID: subAccountID, PickupID: PickupID };
+      const options = { replacements: replacements, type: Sequelize.QueryTypes.SELECT };
+      const result = await sequelize.query(query, options);
+      return result as unknown as PickupsModel[];
+    } catch (err) {
+      throw new Error(`Could not get AWBs of this Pickup. Error: ${err}`);
+    }
+  }
 }
